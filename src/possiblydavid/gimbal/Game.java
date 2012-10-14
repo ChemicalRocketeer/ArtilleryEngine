2b140521ef73191658061dcadca0bc712d8dd2dc
David Aaron Suddjian <aasuddjian@gmail.com>
2012-10-14 12:21:25 -0700

    First commit
    Added Game class.
    It currently displays a JFrame with a black background and randomly
    generated rectangles.

---
+++ src/possiblydavid/gimbal/Game.java
@@ -0,0 +1,100 @@
+package possiblydavid.gimbal;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.util.Random;
+
+import javax.swing.JFrame;
+
+/**
+ * The Game class displays and runs the game
+ * 
+ * @author David Aaron Suddjian
+ */
+public class Game extends Canvas implements Runnable {
+	private static final long serialVersionUID = 1L;
+
+	public static int width = 800;
+	public static int height = width / 16 * 10;
+	public static String title = "Gimbal";
+
+	private Thread thread;
+	private JFrame frame;
+	boolean running = false;
+
+	public Game() {
+		Dimension size = new Dimension(width, height);
+		setPreferredSize(size);
+
+		frame = new JFrame();
+	}
+
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join(); // end thread thread
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {		
+		while (running) {
+			tick();
+			render();
+		}
+	}
+
+	public void tick() {
+
+	}
+
+	public void render() {
+		BufferStrategy strategy = getBufferStrategy();
+		if (strategy == null) {
+			createBufferStrategy(3);
+			return;
+		}
+
+		Graphics g = strategy.getDrawGraphics();
+
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		// generate colored test rectangles
+		Random rand = new Random(System.nanoTime());
+		g.setColor(new Color(rand.nextInt(255) + 1, rand.nextInt(255) + 1, rand.nextInt(255) + 1));
+		int xPos = rand.nextInt(getWidth());
+		int yPos = rand.nextInt(getHeight());
+		g.fillRect(xPos, yPos, rand.nextInt(getWidth() - xPos), rand.nextInt(getHeight() - yPos));
+
+		g.dispose();
+
+		strategy.show();
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+
+		game.frame.setResizable(false);
+		game.frame.setTitle(title);
+		game.frame.add(game);
+		game.frame.pack(); // automatically set size
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null); // center
+		game.frame.setVisible(true);
+
+		game.start();
+	}
+
+}
