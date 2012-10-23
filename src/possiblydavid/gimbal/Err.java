c0227f5c84b9301c14c7489b173747f94d675d18
David Aaron Suddjian <aasuddjian@gmail.com>
2012-10-22 21:55:59 -0700

    ADD Err class to print errors to console
    Objects can use Err to print standardized error messages. Each message
    has an ID number, which is used when calling it. This will make it
    easier to standardize error messages in the future, especially to make
    changes to every error message in the program. If I want to change the
    email contact, I don't want to change every single reference to it
    individually, for example.

---
+++ src/possiblydavid/gimbal/Err.java
@@ -0,0 +1,33 @@
+package possiblydavid.gimbal;
+
+/**
+ * Err handles printing of error messages to the console
+ * 
+ * TODO remove error messages throughout game
+ * 
+ * @author David Aaron Suddjian
+ */
+public class Err {
+	
+	public static final String[] ERRORS = new String[] {
+		"Trying to add null Entity to World!",
+		"Trying to add null Ticker to World!",
+		"LightweightImage can't read the file!",
+		"Can't render a null entity!",
+	};
+	
+	// TODO replace email
+	public static final String APPEND = "\nPlease contact aasuddjian@gmail.com with this error message.\n";
+	
+	/**
+	 * 
+	 * @param id the ID number of the error to print
+	 */
+	public static void error(int id) {
+		if (id >= 0 && id < ERRORS.length) {
+			System.out.println("\nERROR: " + ERRORS[id] + APPEND);
+		} else {
+			System.out.println("\nUnknown error!" + APPEND);
+		}
+	}
+}
