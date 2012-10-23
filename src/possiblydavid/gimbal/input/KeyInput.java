f0a3d6aa9e952b4f67795ab25b49a7532e249c7f
David Aaron Suddjian <aasuddjian@gmail.com>
2012-10-23 00:28:41 -0700

    COMMENT KeyInput

--- src/possiblydavid/gimbal/input/KeyInput.java
+++ src/possiblydavid/gimbal/input/KeyInput.java
@@ -8,13 +8,17 @@ import possiblydavid.gimbal.Tick;
 /**
  * KeyInput stores keyboard input and can be accessed by other objects that need to know whether a key is pressed or not.
  * 
+ * TODO improve implementation
+ * 
  * @since 10-23-12
  * @author David Aaron Suddjian
  *
  */
 public class KeyInput implements KeyListener, Tick{
 	
+	// All possible keys (but not actually all the POSSIBLE keys because that would be an immense array)
 	private boolean[] keys = new boolean[120];
+	// The keys that are relevant to this program
 	public boolean up, down, left, right;
 	
 	public void tick() {
@@ -23,11 +27,27 @@ public class KeyInput implements KeyListener, Tick{
 		left = keys[KeyEvent.VK_A] || keys[KeyEvent.VK_DOWN];
 		right = keys[KeyEvent.VK_D] || keys[KeyEvent.VK_RIGHT];
 	}
+	
+	/**
+	 * Returns whether a key is pressed or not.
+	 * 
+	 * @param e the Key you want to know about
+	 * @return true if the key is pressed, else false
+	 */
+	public boolean pressed(KeyEvent e) {
+		return keys[e.getKeyCode()];
+	}
 
+	/**
+	 * Called by JVM when a key is pressed. Do not call this method unless you are making a robot.
+	 */
 	public void keyPressed(KeyEvent e) {
 		keys[e.getKeyCode()] = true;
 	}
 
+	/**
+	 * Called by JVM when a key is released. Do not call this method unless you are making a robot.
+	 */
 	public void keyReleased(KeyEvent e) {
 		keys[e.getKeyCode()] = false;
 	}
