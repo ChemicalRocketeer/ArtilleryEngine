69e7301cb898babaeb44c0f9481dedbcc8d816b8
David Aaron Suddjian <aasuddjian@gmail.com>
2012-10-23 22:46:42 -0700

    @version Pre-Alpha.0.03

--- src/possiblydavid/gimbal/Game.java
+++ src/possiblydavid/gimbal/Game.java
@@ -17,7 +17,7 @@ import possiblydavid.gimbal.world.testWorld;
 /**
  * The Game handles display and management of game objects.
  * 
- * @version Pre-Alpha.0.02_2
+ * @version Pre-Alpha.0.03
  * @since 10-14-12
  * @author David Aaron Suddjian
  */
