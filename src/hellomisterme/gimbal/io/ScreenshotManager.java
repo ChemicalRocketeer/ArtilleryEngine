0133f2dddeae52c2c5a4c7b28847c23919fd84df
David Aaron Suddjian <aasuddjian@gmail.com>
2012-11-14 16:57:22 -0800

    Refactor screenshotManager
    The names of ScreenshotManager variables are just number and prefix
    (instead of screenshotNumber and screenshotPrefix as they were when
    Render handled screenshots).

--- src/hellomisterme/gimbal/io/ScreenshotManager.java
+++ src/hellomisterme/gimbal/io/ScreenshotManager.java
@@ -17,8 +17,8 @@ import javax.imageio.ImageIO;
  */
 public class ScreenshotManager {
 
-	private int screenshotNumber = 0;
-	private String screenshotPrefix = "screen ";
+	private int number = 0;
+	private String prefix = "screen ";
 
 	public ScreenshotManager() {
 		readScreenshotNumber();
@@ -33,11 +33,11 @@ public class ScreenshotManager {
 		for (int i = 0; i < pixels.length; i++) {
 			shot[i] = pixels[i];
 		}
-		screenshotNumber++;
+		number++;
 		Calendar c = Calendar.getInstance();
 		String fileName = c.get(Calendar.MONTH) + 1 + "-" + c.get(Calendar.DAY_OF_MONTH) + "-" + c.get(Calendar.YEAR) + ""; // date
 		fileName += " " + c.get(Calendar.HOUR_OF_DAY) + "_" + c.get(Calendar.MINUTE) + "_" + c.get(Calendar.SECOND) + ""; // time
-		fileName = "screenshots/" + screenshotPrefix + screenshotNumber + "  " + fileName + ".png"; // other file name stuff
+		fileName = "screenshots/" + prefix + number + "  " + fileName + ".png"; // other file name stuff
 		try {
 			File outFile = new File(fileName);
 			outFile.mkdirs();
@@ -55,27 +55,27 @@ public class ScreenshotManager {
 	public void readScreenshotNumber() {
 		File[] shots = new File("screenshots").listFiles();
 		if (shots == null) {
-			screenshotNumber = 0;
+			number = 0;
 			return;
 		}
 		for (int i = 0; i < shots.length; i++) {
 			String name = shots[i].getName();
-			int index = name.indexOf(screenshotPrefix); // find the screenshotPrefix in file
-			index += screenshotPrefix.length();
+			int index = name.indexOf(prefix); // find the prefix in file
+			index += prefix.length();
 			if (index != -1) {
 				name = name.substring(index); // name now starts with its screenshot number
-				index = name.indexOf(' '); // now use index to find the end of the number
+				index = name.indexOf(' '); // now use frame to find the end of the number
 				if (index != -1) {
 					name = name.substring(0, index);
 					BigInteger num = new BigInteger(name);
-					if (num.intValue() > screenshotNumber) {
-						screenshotNumber = num.intValue();
+					if (num.intValue() > number) {
+						number = num.intValue();
 					}
 				} else {
-					System.out.println("Render.readScreenshotNumber: " + name + " is an illegal filename!"); // TODO remove/modify
+					System.out.println("Render.readScreenshotNumber: " + name + " ends illegally!"); // TODO remove/modify
 				}
 			} else {
-				System.out.println("Render.readScreenshotNumber: " + name + " is an illegal filename!"); // TODO remove/modify
+				System.out.println("Render.readScreenshotNumber: " + name + " begins illegally!"); // TODO remove/modify
 			}
 
 		}
